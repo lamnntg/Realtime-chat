4c0c3d4dd4b6fd4a020def9a3b790c0e391ddee4
lamylov3 <lamnguyen.hust@gmail.com>
2021-09-28 00:04:25 +0700

    update useMemo

--- src/components/ChatRoom/RoomList/RoomList.js
+++ src/components/ChatRoom/RoomList/RoomList.js
@@ -2,10 +2,22 @@ import { Button, Collapse, Typography } from 'antd'
 import React from 'react';
 import './RoomList.scss';
 import { PlusSquareOutlined, CaretRightOutlined } from "@ant-design/icons"
-
+import { useFirestore } from "./../../../hooks/useFirestore";
+import { AuthContext } from "../../../Contexts/AuthProviderContext";
 const { Panel } = Collapse;
 
 export default function RoomList() {
+  const { user } = useContext(AuthContext)
+
+  const roomCondition = useMemo(() => {
+    return  {
+      fieldsName: "members",
+      operator: 'array-contains',
+      compareValue: user.uid
+    }
+  }, [user.uid])
+  const rooms = useFirestore("rooms", roomCondition)
+
   return (
     <Collapse className="room-list" defaultActiveKey={['1']}>
       <Panel className="room-list__panel" header="List Room" key='1' style={{color:"white"}}>
--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,33 +1,34 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React from "react";
+import { useEffect, useState } from "react";
 import { collection, onSnapshot } from "firebase/firestore";
 import { db } from "../firebase/config";
 
-export default function useFirestore(collection, condition) {
-  const { rooms, setRooms } = useState([]);
+export default function useFirestore(collectionRef, condition) {
+  const [document, setDocument] = useState([]);
+
   useEffect(() => {
-    /**
-     * {
-     *  fields name: 'abc'
-     *  operator: "=="
-     *  compareValue: 'cde'
-     * }
-     * 
-     */
-    const queryDB = collection(db, "users");
+    let queryDB = collection(db, collectionRef);
     if (condition) {
       if (!condition.compareValue || !condition.compareValue.length) {
-        return;        
+        return;
       }
-      const queryDB = query(collection(db, "users"), where(condition.fieldsName, condition.Operator, condition.compareValue))
+      const queryDB = query(
+        collection(db, collectionRef),
+        where(condition.fieldsName, condition.Operator, condition.compareValue)
+      );
     }
 
     const unsubscribe = onSnapshot(queryDB, (querySnapshot) => {
-      const rooms = [];
+      const documents = [];
       querySnapshot.forEach((doc) => {
-          rooms.push(doc.data());
+        rooms.push(doc.data());
       });
-      console.log("Current rooms : ", rooms.join(", "));
+      setDocument(documents);
+      console.log("Current documents : ", documents.join(", "));
     });
-  }, [rooms])
+
+    return unsubscribe();
+  }, [collectionRef, condition]);
+
+  return document;
 }
